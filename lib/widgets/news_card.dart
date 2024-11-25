d82a89e5be247dce61430e01be36e8063b9ef060
Mohamed Ijlal <ijlalm@treinetic.com>
2024-11-25 16:36:36 +0530

    BUG FIXED

--- lib/widgets/news_card.dart
+++ lib/widgets/news_card.dart
@@ -24,9 +24,25 @@ class NewsCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            news.urlToImage.isNotEmpty
-                ? Image.network(news.urlToImage, fit: BoxFit.cover)
-                : Container(height: 150, color: Colors.grey),
+            Image.network(
+              news.urlToImage,
+              loadingBuilder: (BuildContext context, Widget child,
+                  ImageChunkEvent? loadingProgress) {
+                if (loadingProgress == null) return child;
+                return Center(
+                  child: CircularProgressIndicator(
+                    value: loadingProgress.expectedTotalBytes != null
+                        ? loadingProgress.cumulativeBytesLoaded /
+                            (loadingProgress.expectedTotalBytes ?? 1)
+                        : null,
+                  ),
+                );
+              },
+              errorBuilder:
+                  (BuildContext context, Object error, StackTrace? stackTrace) {
+                return Image.asset('assets/images/placeholder.png');
+              },
+            ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
